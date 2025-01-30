d62f2e24950b3822c2367a42565236e4057e01af
efdubya4 <90352730+efdubya4@users.noreply.github.com>
2025-01-30 17:38:21 -0500

    add name in comment

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,6 +1,7 @@
 package src.org.howard.edu.lsp.assignment1;
-
-
+/*
+Foster Ware IV
+*/
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
